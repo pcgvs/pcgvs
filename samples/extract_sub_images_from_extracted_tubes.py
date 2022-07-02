4c2ced835531a35e1cffff92c613266a3e93ef33
Gigi-G <seminara.luigi@gmail.com>
2022-07-02 02:06:32 +0200

    feat: samples

---
+++ samples/extract_sub_images_from_extracted_tubes.py
@@ -0,0 +1,3 @@
+from pcgvs.extraction import extract_sub_images
+
+extract_sub_images(path_tubes="../notebooks/data/video-1.txt", media_path="../notebooks/videos/video-1-raw.mp4")
---
+++ samples/extract_tubes.py
@@ -0,0 +1,3 @@
+from pcgvs.extraction import extract_tubes
+
+extract_tubes(source="../notebooks/videos/video-1-raw.mp4", yolo_weights="yolov5m6.pt", strong_sort_weights="osnet_x1_0_market1501.pt")
