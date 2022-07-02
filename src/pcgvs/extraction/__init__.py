30174736d5e6132a54eac63c8285c76100b14f2e
Gigi-G <seminara.luigi@gmail.com>
2022-07-02 02:45:38 +0200

    update

--- src/pcgvs/extraction/__init__.py
+++ src/pcgvs/extraction/__init__.py
@@ -1,4 +1,13 @@
+import cv2
 import numpy as np
+import os
+import sys
+
+from pathlib import Path
+from pcgvs.extraction.track import run
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # yolov5 strongsort root directory
 
 class Tube: 
     
@@ -50,4 +59,72 @@ class Tube:
     
     def __str__(self):
         return self.tag
+
+    
+def extract_tubes(source, yolo_weights, strong_sort_weights):
+    if source == '' or source == '0':
+        print("ERROR: you have to specify a correct video path")
+        sys.exit(-1)
+    run(
+        source=source, 
+        yolo_weights=yolo_weights, 
+        strong_sort_weights=strong_sort_weights
+    )
+
+
+def _create_frames_dictionary(source_tubes):
+    frames = {}
+    with open(source_tubes, 'r') as f:
+        for line in f:
+            f, id, x, y, w, h = line.split()[0:6]
+            if int(f) not in frames.keys():
+                frames[int(f)] = []
+            frames[int(f)].append([id, int(x), int(y), int(w), int(h)])
+    return frames
+
+
+def extract_sub_images(path_tubes, media_path):
+    frames = _create_frames_dictionary(path_tubes)
+    cap = cv2.VideoCapture(media_path)
+    folder_name = str(ROOT / (media_path.split("/")[-1])[:-4])
+    
+    try:
+        os.mkdir(folder_name)
+    except:
+        print(f"ERROR: {folder_name} already exist")
+        sys.exit(-1)
+    
+    ret = True
+    num_frame = 1
+    while ret:
+        ret, frame = cap.read()
+
+        if num_frame in frames.keys():
+            for id, x, y, w, h in frames[num_frame]:
+                ROI = frame[y:y+h, x:x+w].copy()
+                cv2.imwrite(folder_name + "/" + str(id) + "_" + str(num_frame) + '.jpg', ROI)
         
+        num_frame += 1
+
+
+def extract_background(path_tubes, media_path):
+    frames = _create_frames_dictionary(path_tubes)
+    cap = cv2.VideoCapture(media_path)
+    
+    ret = True
+    background = True
+    count_bg = 0
+    num_frame = 1
+    while ret:
+        ret, frame = cap.read()
+
+        if num_frame in frames.keys():
+            count_bg = 0
+        elif background and count_bg > 8:
+            background = False
+            cv2.imwrite(str(ROOT / ((media_path.split("/")[-1])[:-4] + "_background.jpg")), frame)
+            ret = False
+        else:
+            count_bg += 1
+        
+        num_frame += 1
